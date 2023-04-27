c35639015ba354149debfb6235572e330c323bbf
Maksim Bondarenko <m.bondarenko1988@yandex.ru>
2023-04-27 23:02:04 +0300

    Done forst homework to fourth seminar about java

---
+++ homework_1.java
@@ -0,0 +1,30 @@
+package Seminar_3_HW;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class homework_1 {
+    public static void main(String[] args) {
+        List<Integer> list = createList(10, 0,50);
+        System.out.println(list);
+        changeList(list);
+    }
+
+    public static List<Integer> createList(int capacity, int min, int max){
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < capacity; i++) {
+            list.add((int)(Math.random() * (max - min + 1) + min));
+        }
+        return list;
+    }
+
+    static void changeList (List<Integer> list){
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) % 2 == 0){
+                list.remove(i);
+                i--;
+            }
+        }
+        System.out.println(list);
+    }
+}
